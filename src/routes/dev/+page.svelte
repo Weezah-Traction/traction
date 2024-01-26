ad856225a3436d1d45099bd225365d4429dee44b
ltm59 <ltm59@drexel.edu>
2024-01-26 14:11:16 -0500

    Liam's First Crack At It
    - edited some routing language in the dev page that calls NavigationBar and NavigationBarButton
    - tried making Primary Button component (NEEDS REVIEW)

--- src/routes/dev/+page.svelte
+++ src/routes/dev/+page.svelte
@@ -1,9 +1,17 @@
 <script>
-	import NavigationBar from "../../lib/NavigationBar.svelte";
-import NavigationBarButton from "../../lib/NavigationBarButton.svelte";
+	import NavigationBar from '$lib/NavigationBar.svelte';
+	import NavigationBarButton from '$lib/NavigationBarButton.svelte';
+	import PrimaryButton from '$lib/PrimaryButton.svelte';
+	import SmallWidget from '$lib/SmallWidget.svelte';
 </script>
 
 
 <h1>Development Test</h1>
 
+<PrimaryButton type="primary">
+	<p slot="text"></p>
+</PrimaryButton>
+<SmallWidget></SmallWidget>
 <NavigationBar></NavigationBar>
+
+

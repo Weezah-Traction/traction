5427fae24096416bd73ca6b738ae42cf1c931605
Roger Korpics <rmk347@drexel.edu>
2024-01-27 18:32:00 -0500

    :construction: Working on 100-Level Componants
    -Started work on the 100-level components.
    
    - Built out the Pill Button

---
+++ src/lib/PillButton.svelte
@@ -0,0 +1,10 @@
+<slot>
+    <button>Button</button>
+</slot>
+
+<style>
+    button{
+        padding: 1em;
+        border-radius: 5em;
+    }
+</style>
--- src/routes/dev/+page.svelte
+++ src/routes/dev/+page.svelte
@@ -2,6 +2,7 @@
 	import Button from "../../lib/Button.svelte";
   import NavigationBar from "../../lib/NavigationBar.svelte";
   import NavigationBarButton from "../../lib/NavigationBarButton.svelte";
+	import PillButton from "../../lib/PillButton.svelte";
 	import TextSubmission from "../../lib/TextSubmission.svelte";
 </script>
 
@@ -13,3 +14,4 @@
 <TextSubmission purpose=fname></TextSubmission>
 <TextSubmission purpose=date></TextSubmission>
 
+<PillButton></PillButton>

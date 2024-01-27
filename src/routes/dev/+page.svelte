0fa8d80d595dd20074ad835495b1e41f46acdf63
Roger Korpics <rmk347@drexel.edu>
2024-01-27 18:16:04 -0500

    :construction: Text Submission Fields
    Added a component for the text submission field.

--- src/routes/dev/+page.svelte
+++ src/routes/dev/+page.svelte
@@ -1,9 +1,14 @@
 <script>
-	import NavigationBar from "../../lib/NavigationBar.svelte";
+	import Button from "../../lib/Button.svelte";
+import NavigationBar from "../../lib/NavigationBar.svelte";
 import NavigationBarButton from "../../lib/NavigationBarButton.svelte";
+	import TextSubmission from "../../lib/TextSubmission.svelte";
 </script>
 
 
 <h1>Development Test</h1>
 
 <NavigationBar></NavigationBar>
+<Button></Button>
+<TextSubmission purpose=fname></TextSubmission>
+<TextSubmission purpose=date></TextSubmission>

2e6603354fe1fe3066d1ab1a5e002404c00c42fb
Roger Korpics <rmk347@drexel.edu>
2024-01-30 22:22:41 -0500

    :clipboard: Added Basic Plan 100 Levels
    - Added components for the basic plan text and thumbnail.
    
    - Added a purple jpg to use for testing.

---
+++ src/lib/BasicPlanThumbnail.svelte
@@ -0,0 +1,11 @@
+<script>
+    let link = 'link'
+    let thumb = '$lib/assets/testPurple.jpg';
+    let alt = 'A solid purple block.'
+</script>
+
+<slot>
+    <a href={link}>
+        <img alt={alt} src={thumb}>
+    </a>
+</slot>

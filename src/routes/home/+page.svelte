e806480bcb8b698aa120a687e44d975cac71e8c2
Roger Korpics <rmk347@drexel.edu>
2024-02-09 00:14:51 -0500

    :construction: Added Pre-Run to Home
    - Added the Pre Run checklist component to the home page.

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,6 +2,7 @@
 	import BasicPlanItem from "$lib/BasicPlanItem.svelte";
     import Header from "$lib/Header.svelte";
     import NavigationBar from "$lib/NavigationBar.svelte";
+    import PreRunChecklist from "$lib/PreRunChecklist.svelte"
     import SmallestWidget from "$lib/SmallestWidget.svelte"
 	import MapWidget from "../../lib/MapWidget.svelte";
 	import RunButton from "../../lib/RunButton.svelte";
@@ -11,6 +12,7 @@
 <Header></Header>
 <BasicPlanItem></BasicPlanItem>
 <MapWidget></MapWidget>
+<PreRunChecklist></PreRunChecklist>
 <SmallestWidget></SmallestWidget>
 <SmallestWidget></SmallestWidget>
 <RunButton></RunButton>

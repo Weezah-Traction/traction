f5415000cd9f88330ca60c56a9ead1255f681b89
ltsbryant1125 <bl629@drexel.edu>
2024-02-08 23:04:00 -0500

    :art: Updated run page
    Added in components to the run page, including new buttons/icons.

---
+++ src/lib/RunButtons.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let type = 'Type'; // Declares the property
+    let icon;
+
+    import stopIcon from '$lib/assets/btnStop.svg';
+    import pauseIcon from '$lib/assets/btnPause.svg';
+    
+    let alt = "Alt Text";
+
+    if (type == "stop"){
+        alt = "Stop Button"
+        icon = stopIcon;
+    }
+
+    if (type == "pause"){
+        alt = "Pause Button"
+        icon = pauseIcon;
+    }
+
+</script>
+
+<slot>
+    <button class={type}><img src={icon} alt={alt}></button>
+</slot>
+
+<style>
+    button {
+        border-style: none;
+        background-color: transparent;
+    }
+</style>
--- src/routes/run/+page.svelte
+++ src/routes/run/+page.svelte
@@ -1,5 +1,11 @@
 <script>
     import BlankHeader from "../../lib/BlankHeader.svelte";
+    import SmallestWidget from "../../lib/SmallestWidget.svelte";
+    import RunButtons from "../../lib/RunButtons.svelte";
 </script>
 
 <BlankHeader></BlankHeader>
+<SmallestWidget></SmallestWidget>
+<SmallestWidget></SmallestWidget>
+<RunButtons type="stop"></RunButtons>
+<RunButtons type="pause"></RunButtons>

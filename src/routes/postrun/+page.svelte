bea4904317751f866c53b5c92bda5719085c50ba
ltsbryant1125 <bl629@drexel.edu>
2024-02-09 00:14:03 -0500

    :construction: PostRun Page
    Built out content on post-run page using HTML and CSS

---
+++ src/routes/postrun/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+
+</script>
+
+<slot>
+    <div class="container">
+        <div class="main">
+            <h1>Amazing Job!</h1>
+            <img src="../../lib/assets/gator.svg" alt="Crocodile">
+            <h5>Congratulations on your first run!</h5>
+            <p>You are now a part of a community of millions of people that are making a positive change on their mind and body through the simple act of running!</p>
+        </div>
+        <div class="continue">
+            <a href="">Continue</a>
+        </div>
+    </div>
+</slot>
+
+<style>
+    .container {
+        background-color: var(--lavender-100);
+        height: 852px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .main {
+        display: flex;
+        flex-direction: column;
+        width: 353px;
+        margin: 20px;
+        padding: 150px 0px 300px 0px;
+    }
+
+    h1 {
+        font-family: 'Fugaz One';
+        font-size: 47.78px;
+        margin: 10px;
+    }
+
+    h5 {
+        font-family: 'Poppins';
+        font-size: 23.04px;
+        margin: 10px;
+    }
+
+    p {
+        font-family: 'Poppins';
+        font-size: 16px;
+        margin: 10px;
+    }
+
+    .continue {
+        width: 353px;
+        height: 44px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: var(--lavender-300);
+        border-radius: 40px;
+    }
+    
+    a {
+        text-decoration: none;
+        font-family: 'Poppins';
+        color: var(--lavender-700);
+    }
+</style>

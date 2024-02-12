5936130018e0eeda5592d4473af690b2db55be99
TusharRai99 <72166628+TusharRai99@users.noreply.github.com>
2024-02-12 18:32:46 -0500

    Play Button

---
+++ src/lib/PlayButton.svelte
@@ -0,0 +1,11 @@
+<script>
+    import btn_Play from '$lib/assets/btn_Play.svg';
+</script>
+
+<img class="playButton" src=playButton alt="Play Button">
+
+<style>
+    .playButton {
+         z-index: 1;
+    }
+</style>

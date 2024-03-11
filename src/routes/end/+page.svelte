c0d32f643a534b0f41f3db7d01f300d61ef624fc
ltsbryant1125 <bl629@drexel.edu>
2024-03-10 23:22:48 -0400

    :construction: added functionality to "End" button
    Created a route for the end button that would open up the modal component. The buttons in said component also function appropriately

---
+++ src/routes/end/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import ModalComponent from "../../lib/ModalComponent.svelte";
+</script>
+
+<slot>
+    <div>
+        <ModalComponent></ModalComponent>
+    </div>
+</slot>
+
+<style>
+
+</style>

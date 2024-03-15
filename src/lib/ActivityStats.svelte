23276417202a7fbf6cb35853e28fe63f8c499847
ltm59 <ltm59@drexel.edu>
2024-03-14 23:01:21 -0400

    :crown: Activity Stats fill w content!

--- src/lib/ActivityStats.svelte
+++ src/lib/ActivityStats.svelte
@@ -1,27 +1,89 @@
 <script>
-     export let miles = "0.0";
-     export let runs = "0";
-     export let avgPace = "0'0";
-     export let time = "0:00";
+     // export let miles = "0.0";
+     // export let runs = "0";
+     // export let avgPace = "0'0";
+     // export let time = "0:00";
+     
+     import { runs } from '$lib/data';
+
+     // Add Up Distance
+
+     const runs_dist = runs.map((i) => i.distance)
+     console.log(runs_dist);
+
+     let dist_sum = 0;
+     for (let i = 0; i < runs_dist.length; i++) {
+     dist_sum += runs_dist[i];
+     }
+
+     console.log(dist_sum);
+
+
+
+     // Get Average Pace
+
+     const runs_pace = runs.map((i) => i.pace)
+     console.log(runs_pace);
+
+     let pace_sum = 0;
+     for (let i = 0; i < runs_pace.length; i++) {
+     pace_sum += runs_pace[i];
+
+     }
+
+     var pace_avg = pace_sum / runs_pace.length;
+
+     console.log(pace_avg);
+
+
+     
+     // Get Total Time
+
+     /* Time is expressed in milliseconds. 60000 milliseconds = 1 minute */
+
+     const runs_time = runs.map((i) => i.duration);
+     console.log(runs_time);
+
+     let time_sum = 0;
+     for (let i = 0; i < runs_time.length; i++) {
+     time_sum += runs_time[i];
+     }
+
+     console.log(time_sum);
+
+     let time_minutes = (Math.floor((time_sum % (1000 * 60 * time_sum)) / (1000 * 60)));
+     let time_seconds = (Math.floor((time_sum% (1000 * 60)) / 1000));
+
+     console.log(time_minutes);
+     console.log(time_seconds);
+
+     // console.log(time_combine);
+
+
 </script>
 
 <slot>
      <div class="activityStats">
           <div class="milesContainer">
-               <h3 class="Fugaz">{miles}</h3>
+               <h3 class="Fugaz">{dist_sum}</h3>
                <p>Miles</p>
           </div>
           <div class="otherStats">
                <div class="statContainer">
-                    <h5 class="Fugaz">{runs}</h5>
+                    <h5 class="Fugaz">{runs_dist.length}</h5>
                     <p>Runs</p>
                </div>
                <div class="statContainer">
-                    <h5 class="Fugaz">{avgPace}</h5>
+                    <div class="statBox">
+                         <h5 class="Fugaz">{pace_avg}</h5>
+                    </div>
                     <p>Avg. Pace</p>
                </div>
                <div class="statContainer">
-                    <h5 class="Fugaz">{time}</h5>
+                    <div class="statBox">
+                         <h5 class="Fugaz">{time_minutes}:</h5>
+                         <h5 class="Fugaz">{time_seconds}</h5>
+                    </div>
                     <p>Time</p>
                </div>
           </div>
@@ -45,6 +107,11 @@
           display: flex;
           flex-direction: column;
           align-items: center;
+     }
+
+     .statBox {
+          display: flex;
+          justify-content: space-between;
      }
 
      .otherStats {
